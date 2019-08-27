52f55c3d7b4c7385ee77c63dbeaf554a1c8eec96
meechaguep <53546764+meechaguep@users.noreply.github.com>
2019-08-26 22:40:16 -0400

    Add files via upload

---
+++ 26082019/000536.py
@@ -0,0 +1,24 @@
+#diccionarios
+
+goles_1era_division={} 
+
+goles_1era_division["Esteban Paredes"]=215
+goles_1era_division["Milovan"]= 2
+goles_1era_division["herrera"]=-215
+
+print goles_1era_division #imprime el diccionario
+#lo siguiente imprime valores del diccionario
+print goles_1era_division["Esteban Paredes"]
+print goles_1era_division["Milovan"]
+print goles_1era_division["herrera"]
+
+
+#intento 2
+
+print "puede ser en base a numeros"
+
+goles_1era_division[7]=215
+print goles_1era_division[7]
+
+
+
