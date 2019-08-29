55a5f92e3ffa0559bdf49d319ffd4c0d0d23f83b
meechaguep <53546764+meechaguep@users.noreply.github.com>
2019-08-29 00:05:50 -0400

    Add files via upload

---
+++ 28082019/000934.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+a1= np.array([3,14,19])
+print a1
+
+a2= np.arange(41,56,3)#int,cada 3
+print a2
+
+a3=np.linspace(41,56,4)#float, 5 elementos
+print a3
+
+a4= a3.reshape(2,2)#lcambiamos a3 por una matriz de 2x2
+print a4
+print a3 #a3 sigue igual, a4 es una variable nueva
+
+#intento 2 
+
+print a3.size #cantidad de elementos
+print a3.shape#nos muestra la composicion
+print a3.dtype #indica el tipo de datos
+print a3.itemsize #bits
+
+
+print a4.size 
+print a4.shape
+print a4.dtype
+print a4.itemsize
+
